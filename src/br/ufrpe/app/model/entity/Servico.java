a765da823e6e78530ae4b7dd035cba0abc42a22d
Vanessa Cristina <vanessa.cs.oliveira@gmail.com>
2018-11-19 23:34:26 -0200

    Criação da classe 'Serviço'

---
+++ src/br/ufrpe/app/model/entity/Servico.java
@@ -0,0 +1,70 @@
+package br.ufrpe.app.model.entity;
+
+public class Servico {
+	private String nome;
+	private String descricao;
+	
+	public Servico() {
+	
+	}
+	
+	public Servico(String nome, String descricao) {
+		super();
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Servico other = (Servico) obj;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Servico [nome=" + nome + ", descricao=" + descricao + "]";
+	}
+	
+
+}
