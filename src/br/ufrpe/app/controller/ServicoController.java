00763deef17dd1ead48ba11fc98df3877d876b3a
Vanessa Cristina <vanessa.cs.oliveira@gmail.com>
2018-11-26 23:43:32 -0200

    Att ServicoController

--- src/br/ufrpe/app/controller/ServicoController.java
+++ src/br/ufrpe/app/controller/ServicoController.java
@@ -1,5 +1,31 @@
 package br.ufrpe.app.controller;
 
-public class ServicoController {
+import br.ufrpe.app.model.dao.ServicoDao;
+import br.ufrpe.app.model.dao.daoImpl.ServicoDaoImpl;
+import br.ufrpe.app.model.entity.Servico;
 
+public class ServicoController {
+	
+	ServicoDao servicoDao = new ServicoDaoImpl();
+	
+	public Servico[] acharTudo() {
+		return servicoDao.acharTudo();
+	}
+
+	public Servico acharPeloNome(String nome) {
+		return servicoDao.acharPeloNome(nome);
+	}
+	
+	public void criar(Servico servico) {
+		servicoDao.criar(servico);
+	}
+
+	public void atualizar(Servico servico) {
+		servicoDao.atualizar(servico);
+	}
+
+	public void deletar(int posicao) {
+		servicoDao.deletar(posicao);
+	}
+	
 }
