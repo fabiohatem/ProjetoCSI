80f157e7b3214b6eedb65f029d02a2321968c9f1
Nicolas Oliveira <sansung700@gmail.com>
2018-11-30 00:48:59 -0200

    Demorou mas chegou!

---
+++ src/br/ufrpe/app/model/dao/daoImpl/OrgaoDaoImpl.java
@@ -0,0 +1,110 @@
+package br.ufrpe.app.model.dao.daoImpl;
+
+import br.ufrpe.app.model.dao.OrgaoDao;
+import br.ufrpe.app.model.entity.Orgao;
+import br.ufrpe.app.model.repository.OrgaoRepository;
+
+public class OrgaoDaoImpl implements OrgaoDao {
+
+	private int totalDeOrgaos = 0;
+
+	public OrgaoDaoImpl() {
+		OrgaoRepository.iniciaRepository();
+		totalDeOrgaos = OrgaoRepository.totalDeOrgaosInicial;
+	}
+
+	@Override
+	public Orgao[] findAll() {
+		return OrgaoRepository.orgaos;
+	}
+
+	@Override
+	public Orgao findSigla(String sigla) {
+		Orgao result = null;
+		for (int i = 0; i < totalDeOrgaos; i++) {
+			if (OrgaoRepository.orgaos[i].getSigla().equals(sigla)) {
+				result = OrgaoRepository.orgaos[i];
+			}
+		}
+		return result;
+	}
+
+	@Override
+	public Orgao findPosicao(int posicao) {
+		return OrgaoRepository.orgaos[posicao];
+	}
+
+	@Override
+	public boolean contem(Orgao orgao) {
+		for (int i = 0; i < this.totalDeOrgaos; i++) {
+			if (orgao.equals(OrgaoRepository.orgaos[i])) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public int tamanho() {
+		return this.totalDeOrgaos;
+	}
+
+	@Override
+	public void criar(Orgao orgao) {
+		this.garantaEspaco();
+		OrgaoRepository.orgaos[this.totalDeOrgaos] = orgao;
+		this.totalDeOrgaos++;
+	}
+
+	@Override
+	public void criar(int posicao, Orgao orgao) {
+		this.garantaEspaco();
+		if (!this.posicaoValida(posicao)) {
+			throw new IllegalArgumentException("Posição inválida");
+		}
+		for (int i = this.totalDeOrgaos - 1; i >= posicao; i--) {
+			OrgaoRepository.orgaos[i + 1] = OrgaoRepository.orgaos[i];
+		}
+		OrgaoRepository.orgaos[posicao] = orgao;
+		this.totalDeOrgaos++;
+	}
+
+	@Override
+	public void update(Orgao orgao) {
+		for (int i = 0; i < this.totalDeOrgaos; i++) {
+			if (orgao.getSigla().equals(OrgaoRepository.orgaos[i].getSigla())) {
+				OrgaoRepository.orgaos[i] = orgao;
+			}
+		}
+	}
+
+	@Override
+	public void delete(int posicao) {
+		if (!this.posicaoOcupada(posicao)) {
+			throw new IllegalArgumentException("Posição inválida");
+		}
+		for (int i = posicao; i < this.totalDeOrgaos - 1; i++) {
+			OrgaoRepository.orgaos[i] = OrgaoRepository.orgaos[i + 1];
+		}
+		this.totalDeOrgaos--;
+	}
+
+	private boolean posicaoOcupada(int posicao) {
+		return posicao >= 0 && posicao < this.totalDeOrgaos;
+	}
+
+	private boolean posicaoValida(int posicao) {
+		return posicao >= 0 && posicao <= this.totalDeOrgaos;
+	}
+
+	private void garantaEspaco() {
+		if (this.totalDeOrgaos == OrgaoRepository.orgaos.length) {
+			Orgao[] novoArray = new Orgao[OrgaoRepository.orgaos.length * 2];
+			for (int i = 0; i < OrgaoRepository.orgaos.length; i++) {
+				novoArray[i] = OrgaoRepository.orgaos[i];
+			}
+			OrgaoRepository.orgaos = novoArray;
+		}
+	}
+	
+}
---
+++ src/br/ufrpe/app/model/entity/Orgao.java
@@ -0,0 +1,63 @@
+package br.ufrpe.app.model.entity;
+
+public class Orgao {
+	
+	private String nome;
+	private String sigla;
+	
+	public Orgao() {
+		
+	}
+	
+	public Orgao(String nome, String sigla) {
+		
+		super();
+		this.nome = nome;
+		this.sigla = sigla;
+		
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSigla() {
+		return sigla;
+	}
+	
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((sigla == null) ? 0 : sigla.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Orgao other = (Orgao) obj;
+		if (sigla == null) {
+			if (other.sigla != null)
+				return false;
+		} else if (!sigla.equals(other.sigla))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Orgao [nome=" + nome + ", sigla=" + sigla + "]";
+	}
+	
+}
