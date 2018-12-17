e4e7368337bc271c3012eda639fefabe474fc32b
FabioHatem <fabiohatem@gmail.com>
2018-12-17 19:34:05 -0300

    usuariocontroler

---
+++ src/br/ufrpe/app/controller/UsuarioController.java
@@ -0,0 +1,34 @@
+package br.ufrpe.app.controller;
+
+
+import br.ufrpe.app.model.dao.UsuarioDao;
+import br.ufrpe.app.model.dao.daoImpl.UsuarioDaoImpl;
+import br.ufrpe.app.model.entity.Usuario;
+
+
+
+public class UsuarioController {
+	
+UsuarioDao usuarioDao = new UsuarioDaoImpl();
+	
+	public Usuario[] findAll() {
+		return usuarioDao.findAll();
+	}
+
+	public Usuario findByLogin(String login) {
+		return usuarioDao.findByLogin(login);
+	}
+	
+	public void criar(Usuario usuario) {
+		usuarioDao.create(usuario);
+	}
+
+	public void atualizar(Usuario usuario) {
+		usuarioDao.update(usuario);
+	}
+
+	public void deletar(int posicao) {
+		usuarioDao.delete(posicao);
+	}
+
+}
