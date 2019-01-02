77a28fa43a2c5cdf6aff50f01f7def2da6a02b29
Vanessa Cristina <vanessa.cs.oliveira@gmail.com>
2019-01-02 20:42:18 -0300

    Colocação do getInstance e o metodo salvarArquivo

--- src/br/ufrpe/app/controller/ServicoController.java
+++ src/br/ufrpe/app/controller/ServicoController.java
@@ -11,23 +11,31 @@ public class ServicoController {
 	ServicoDao servicoDao = new ServicoDaoImpl();
 	
 	public Servico[] findAll() {
+		servicoDao = ServicoDaoImpl.getInstance();
 		return servicoDao.findAll();
 	}
 
 	public Servico findByNome(String nome) {
+		servicoDao = ServicoDaoImpl.getInstance();
 		return servicoDao.findByNome(nome);
 	}
 	
 	public void create(Servico servico) throws ServicoException {
+		servicoDao = ServicoDaoImpl.getInstance();
 		servicoDao.create(servico);
+		servicoDao.salvarArquivo();
 	}
 
 	public void update(Servico servico) {
+		servicoDao = ServicoDaoImpl.getInstance();
 		servicoDao.update(servico);
+		servicoDao.salvarArquivo();
 	}
 
 	public void delete(String nome) {
+		servicoDao = ServicoDaoImpl.getInstance();
 		servicoDao.delete(nome);
+		servicoDao.salvarArquivo();
 	}
 	
 }
